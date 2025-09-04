90eef7ae31bdfc70d57278ee0c7dc6e1a11b6649
Eliseichik <trex666758@gmail.com>
2025-09-04 23:28:44 +0300

    string.c

--- string.c
+++ string.c
@@ -1,5 +1,5 @@
 int main(){
-  char x[4] = "efhg";
-  push(x);
+  char x[4] = "efhg";  // The DWORD PTR [ebp-0x4], 0x67686665 + DWORD PTR [ebp-0x8], 0x00 
+  push(x);             // push string in to the stack
   return 0;
 }
